51d1690a296f3442cfa642d7c0b8ab28b178347f
Fawad Alam <fawadalam5813@gmail.com>
2026-03-02 23:19:04 +0500

    create tiny url clone

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import './App.css'
-import Header from './components/Header'
-import UrlShortener from './components/UrlShortener'
-
+import Header from "./Header/Header";
+import BodyMain1 from "./BodyMain1/BodyMain1";
+import Bodypt2 from "./Body-part-2/Bodypt2";
+import Bodypt3 from "./Body-part-3/Bodypt3";
+import Bodypt4 from "./Body-part-4/Bodypt4";
+import Bodypt5 from "./Body-part-5/Bodypt5";
+import Bodypt6 from "./Body-part-6/Bodypt6";
+import Footer from "./Footer/Footer";
 
 function App() {
   return (
-    <div className='bg-[#ffffff] w-full min-h-screen text-[#E5E5E5]'>
+    <div>
       <Header />
-      <UrlShortener />
+      <BodyMain1 />
+      <Bodypt2 />
+      <Bodypt3 />
+      <Bodypt4 />
+      <Bodypt5 />
+      <Bodypt6 />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from "react-router";
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
 )
