267400d21cbbd2ab926b9d864187823bcceba39f
Fawad Alam <fawadalam5813@gmail.com>
2026-03-03 00:33:51 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Header from "./Header/Header";
 import BodyMain1 from "./BodyMain1/BodyMain1";
-import Bodypt2 from "./Body-part-2/Bodypt2";
-import Bodypt3 from "./Body-part-3/Bodypt3";
-import Bodypt4 from "./Body-part-4/Bodypt4";
-import Bodypt5 from "./Body-part-5/Bodypt5";
-import Bodypt6 from "./Body-part-6/Bodypt6";
+import Bodypt2 from "./Bodypt2/Bodypt2";
+import Bodypt3 from "./Bodypt3/Bodypt3";
+import Bodypt4 from "./Bodypt4/Bodypt4";
+import Bodypt5 from "./Bodypt5/Bodypt5";
+import Bodypt6 from "./Bodypt6/Bodypt6";
 import Footer from "./Footer/Footer";
 
 function App() {
