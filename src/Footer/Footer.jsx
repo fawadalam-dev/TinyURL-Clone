fab1b3bc726079306808000083163ac7bd7bcd6c
Fawad Alam <fawadalam5813@gmail.com>
2026-04-15 14:55:54 +0500

    Update Footer.jsx

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -6,36 +6,14 @@ function Footer() {
     <footer className="footer">
       <div className="footer-content">
         <ul>
-          <h3>Features</h3>
-          <a>Link Editor</a>
-          <a>Link Management</a>
-          <a>Branded Links</a>
-          <a>Short URL Tracking</a>
-          <a>QR Code Generator</a>
-          <a>Short URL API</a>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Blog</li>
+          <li>Careers</li>
+          <li>Terms of Service</li>
+          <li>Privacy Policy</li>
         </ul>
-        <ul>
-          <h3>Resources</h3>
-          <a>Blog</a>
-          <a>For Developers</a>
-          <a>Our Proven Process</a>
-          <a>About Us</a>
-        </ul>
-        <ul>
-          <h3>Contact Us</h3>
-          <a>Help Desk</a>
-          <a>Contact Sales</a>
-          <a>Contact Support</a>
-          <a>Report Abuse</a>
-        </ul>
-        <ul>
-          <h3>Legal</h3>
-          <a>Terms of Service</a>
-          <a>Privacy Policy</a>
-          <a>Cookie Policy</a>
-          <a>Accessibility Statement</a>
-          <a>Privacy Manager</a>
-        </ul>
+       
         <div className="text-right">
           <div>
             <FontAwesomeIcon icon={faFacebookF} />
@@ -43,8 +21,8 @@ function Footer() {
             <FontAwesomeIcon icon={faLinkedin} />
             <FontAwesomeIcon icon={faTwitter} />
             <img src="/mainlogo.png" alt="Logo" />
-            <p>2026 TinyURL LLC</p>
-            <p>All Rights Reserved</p>
+            <p>2026 TinyURL </p>
+           
           </div>
         </div>
       </div>
